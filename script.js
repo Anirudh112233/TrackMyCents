d8447f63f3d3d823c29327dcc289eeb86bf3c094
Anirudh Mishra <anirudhmishra112233@gmail.com>
2024-09-04 22:30:49 +0530

    new project files have been added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
+    const logintoggle = document.getElementById('login-toggle');
+
+    logintoggle.addEventListener('click', function() {
+        window.location.href = 'login.html'; // Redirect to login.html
+    });
+
     const menuToggle = document.getElementById('menu-toggle');
     const sideMenu = document.getElementById('side-menu');
     const closeMenu = document.getElementById('close-menu');
